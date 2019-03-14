b8d31e31eb73d6d1275e310f54a01306821b0725
Pine Wu <octref@gmail.com>
2019-03-14 11:41:41 -0700

    Update

--- test/runTest.ts
+++ test/runTest.ts
@@ -1,20 +1,55 @@
 import * as path from 'path'
 
-import { downloadAndUnzipVSCode } from './vscode-test/download'
-import { runTests } from './vscode-test/run';
+import { runTests, downloadAndUnzipVSCode } from 'vscode-test'
 
 async function go() {
-  const vscodeExecutablePath = await downloadAndUnzipVSCode()
 
-  const extPath = path.resolve(__dirname, '../../')
-  const testPath = path.resolve(__dirname, './suite')
+  const extensionPath = path.resolve(__dirname, '../../')
+  const testRunnerPath = path.resolve(__dirname, './suite')
   const testWorkspace = path.resolve(__dirname, '../../test-fixtures/fixture1')
 
-  const testPath2 = path.resolve(__dirname, './suite2')
+  const testRunnerPath2 = path.resolve(__dirname, './suite2')
   const testWorkspace2 = path.resolve(__dirname, '../../test-fixtures/fixture2')
 
-  await runTests(vscodeExecutablePath, extPath, testPath, testWorkspace)
-  await runTests(vscodeExecutablePath, extPath, testPath2, testWorkspace2)
+  /**
+   * Basic usage
+   */
+  await runTests({
+    extensionPath,
+    testRunnerPath,
+    testWorkspace
+  })
+
+  /**
+   * Running a second test suite
+   */
+  await runTests({
+    extensionPath,
+    testRunnerPath: testRunnerPath2,
+    testWorkspace: testWorkspace2
+  })
+
+  /**
+   * Use 1.31.0 release for testing
+   */
+  await runTests({
+    version: '1.31.0',
+    extensionPath,
+    testRunnerPath,
+    testWorkspace
+  })
+
+  /**
+   * Manually download VS Code and use 1.31.0 release for testing.
+   * Note that if `.vscode-test/vscode-1.31.0` folder exists it won't be re-downloaded
+   */
+  const vscodeExecutablePath = await downloadAndUnzipVSCode('1.31.0')
+  await runTests({
+    vscodeExecutablePath,
+    extensionPath,
+    testRunnerPath,
+    testWorkspace
+  })
 }
 
 go()
